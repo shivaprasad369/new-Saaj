d5387d0b4a930ff343a442a5b9370eb7ac7c28f0
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-02-10 00:32:38 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import About from "./components/About";
+import Services from "./components/Services";
+import Specialization from "./components/Specialization";
+import Client from "./components/Client";
+import Gallary from "./components/Gallary";
+import Footer from "./components/Footer";
+import Cover from "./components/Cover";
+
+function App() {
+  return (
+    <>
+    <Cover/>
+    <div>
+     <About/>
+    </div>
+    <Services/>
+    <Specialization/>
+    <Client/>
+    <Gallary/>
+    <Footer/>
+ {/* <Main/> */}
+    </>
+  );
+}
+
+export default App;
